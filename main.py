301a9ad713b1236c5b246c1b310c832f47b91ab6
Chaim Averbach <Chaymi99@gmail.com>
2025-06-27 14:58:16 +0300

    Add condensed set size tracking and plotting

--- main.py
+++ main.py
@@ -23,12 +23,15 @@ def experiment(X, y):
 
 
     print(f"Starting experiment with classes: {set(y)}")
-    # store results for each (k, p) pair
-    results: dict[tuple[int, int | Literal['∞']], list[float]] = {}
+    # store errors for each (k, p) pair
+    errors: dict[tuple[int, int | Literal['∞']], list[float]] = {}
+    condensed_sizes: dict[tuple[int, int | Literal['∞']], float] = {}
     for k, p in itertools.product(ks, ps):
         # 0: train error, 1: test error - before condensing
         # 2: train error, 3: test error - after condensing
-        results[(k, p)] = [0, 0, 0, 0]
+        errors[(k, p)] = [0, 0, 0, 0]
+        condensed_sizes[(k, p)] = 0
+
 
     # repeat 100 times
     for i in range(100):
@@ -41,31 +44,33 @@ def experiment(X, y):
             classifier = KNNClassifier(k, p)
             # fit it to the data
             classifier.fit(X_train, y_train)
-            # calculate error rates on train and test and add to results
-            results[(k, p)][0] += error_rate(classifier, X_train, y_train)
-            results[(k, p)][1] += error_rate(classifier, X_test, y_test)
-            # condense sets
-            classifier.condense()
-            # again calculate error rates on train and test and add to results
-            results[(k, p)][2] += error_rate(classifier, X_train, y_train)
-            results[(k, p)][3] += error_rate(classifier, X_test, y_test)
+            # calculate error rates on train and test and add to errors
+            errors[(k, p)][0] += error_rate(classifier, X_train, y_train)
+            errors[(k, p)][1] += error_rate(classifier, X_test, y_test)
+            # condense set and add size
+            condensed_sizes[(k, p)] += classifier.condense()
+            # again calculate error rates on train and test and add to errors
+            errors[(k, p)][2] += error_rate(classifier, X_train, y_train)
+            errors[(k, p)][3] += error_rate(classifier, X_test, y_test)
 
     # divide all results by 100 to average over repeated tests
     for k, p in itertools.product(ks, ps):
-        results[(k, p)][0] /= 100
-        results[(k, p)][1] /= 100
-        results[(k, p)][2] /= 100
-        results[(k, p)][3] /= 100
+        errors[(k, p)][0] /= 100
+        errors[(k, p)][1] /= 100
+        errors[(k, p)][2] /= 100
+        errors[(k, p)][3] /= 100
+        condensed_sizes[(k, p)] /= 100
         # optional prints
         # print(f"For k = {k}, p = {p}:")
+        # print(f"Average condensed size: {condensed_sizes[(k, p)]}")
         # print("Before condensing:")
-        # print(f"\tTrain error rate = {results[(k, p)][0]:.4f}")
-        # print(f"\tTest error rate = {results[(k, p)][1]:.4f}")
-        # print(f"\tDifference is {results[(k, p)][1] - results[(k, p)][0]:.4f}")
+        # print(f"\tTrain error rate = {errors[(k, p)][0]:.4f}")
+        # print(f"\tTest error rate = {errors[(k, p)][1]:.4f}")
+        # print(f"\tDifference is {errors[(k, p)][1] - errors[(k, p)][0]:.4f}")
         # print("After condensing:")
-        # print(f"\tTrain error rate = {results[(k, p)][2]:.4f}")
-        # print(f"\tTest error rate = {results[(k, p)][3]:.4f}")
-        # print(f"\tDifference is {results[(k, p)][3] - results[(k, p)][2]:.4f}")
+        # print(f"\tTrain error rate = {errors[(k, p)][2]:.4f}")
+        # print(f"\tTest error rate = {errors[(k, p)][3]:.4f}")
+        # print(f"\tDifference is {errors[(k, p)][3] - errors[(k, p)][2]:.4f}")
 
     # prepare data for plotting
     _x = ks
@@ -75,12 +80,12 @@ def experiment(X, y):
     z_coords = [[], [], [], []]
     for k, p in itertools.product(ks, ps):
         for i in range(4):
-            z_coords[i].append(results[(k, p)][i])
+            z_coords[i].append(errors[(k, p)][i])
 
     # create plots
-    fig, axes = plt.subplots(2, 2, subplot_kw=dict(projection='3d'))
+    errors_fig, axes = plt.subplots(2, 2, subplot_kw=dict(projection='3d'))
     axes = [ax for line in axes for ax in line]
-    fig.suptitle(f'Results for labels {set(y)}', fontsize=16)
+    errors_fig.suptitle(f'Errors for labels {set(y)}', fontsize=16)
 
     axes[0].set_title('Train error rate', fontsize=10)
     axes[1].set_title('Test error rate', fontsize=10)
@@ -99,13 +104,36 @@ def experiment(X, y):
         ax.set_yticklabels(labels)
         ax.set_zlabel('error')
         ax.set_zlim3d(bottom=0, top=None, auto=True)
+
+    # set up plot for condensed set sizes
+    sizes_fig, ax = plt.subplots()
+    # prepare data
+    sizes = []
+    for p in ps:
+        sizes.append(condensed_sizes[(ks[0], p)])
+    # prepare chart
+    ax.set_title(f'Condensed set sizes for labels \n{set(y)}', fontsize=16)
+    ax.bar([3 if p == '∞' else p for p in ps], sizes)
+    ax.set_xlabel('p')
+    ax.set_xticks(_y)
+    labels = [item.get_text() for item in ax.get_xticklabels()]
+    labels[2] = '∞'
+    ax.set_xticklabels(labels)
+    ax.set_ylabel('set size')
+    # make y limits be a little bigger that the range
+    sizes_range = max(sizes) - min(sizes)
+    ax.set_ylim([min(sizes) - sizes_range/2, max(sizes) + sizes_range/2])
+
+    # show all figures
     plt.show()
 
+
     # find the best parameters for test error rate before and after condensing
-    best_uncondensed = min(results, key=lambda tup: results[tup][1])
+    best_uncondensed = min(errors, key=lambda tup: errors[tup][1])
     print(f"Best parameters when uncondensed are k = {best_uncondensed[0]}, p = {best_uncondensed[1]}")
-    best_condensed = min(results, key=lambda tup: results[tup][3])
+    best_condensed = min(errors, key=lambda tup: errors[tup][3])
     print(f"Best parameters when condensed are k = {best_condensed[0]}, p = {best_condensed[1]}")
+    print()
 
 def filter_label_and_extract_X_y(data, label1, label2):
     filtered_data = data.where((data['label'] == label1) | (data['label'] == label2)).dropna()
